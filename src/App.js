0aa4ec10c53d13c525abcc6de6840beeff3a85e8
Charlie Stableford <charlie.stableford@flyporter.com>
2020-12-10 11:37:57 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
 import './App.css';
+import NamesContainer from './NamesContainer';
 
-function App() {
+class App extends React.Component {
 
   state = {
-    illegals: [
+    illegalStates: [
       'North Dakota', 
       'Montana', 
       'Idaho', 
@@ -43,38 +44,44 @@ function App() {
        'Northern Marisana Islands' 
 
     ], 
+    legalState: [
+      'Washington', 
+      'California',
+      'Nevada', 
+      'Hawaii', 
+      'Texas', 
+      'New Mexico',
+      'Colorado'
+    ],
     searchTerm: '' //an empty string controlled by our inpur tag
-  }, 
+  }
 
   editSearchTerm = (e) => {
     this.setState({
       // allows our input field to control the searchTerm value in our state.
       searchTerm: e.target.value
     })
-  }, 
+  }
 
   dynamicSearch = () => {
     //allows us to render what shows up onto the page dynamically.
-    return this.state.illegals.filter(illegal => illegal.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+    return this.state.illegalStates.filter(illegalState => illegalState.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
   }
 
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <p>Where is the 'gay panic defense' legal?</p>
         <p>The 'gay panic defense' is a legal strategy in which a defendant claims they acted in a state of voilent, temporary insanitty, committing assault or murder, because of an unwanted sexual advance from a person in the LGBTQ+ community. </p>
-        <p>Shocked by this? Type in a state that you assume this wouldn't be legal. If it matches this is a law in that state.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Shocked by this? Type in a state name to check what the law is.</p>
+        <input type="text" value = {this.state.searchTerm} onChange= {this.editSearchTerm} placeholder = 'Check a state' /> 
+        <h3></h3>
+        <NamesContainer illegalStates = {this.dynamicSearch()} />       
       </header>
     </div>
   );
+  }
 }
 
 export default App;
